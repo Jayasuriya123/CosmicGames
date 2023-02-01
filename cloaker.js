1ab9d4dd698c5705f1417d5e1fc674b4e0eefd41
Jayasuriya123 <96603454+Jayasuriya123@users.noreply.github.com>
2023-02-01 07:18:33 -0500

    update

--- cloaker.js
+++ cloaker.js
@@ -18,7 +18,7 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.title = "Classes"
         link.rel = "icon";
-        link.href = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Google_Classroom_Logo.svg/512px-Google_Classroom_Logo.svg.png?20221017163738";
+        link.href = "https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png";
         iframe.src = location.href
         style.position = "fixed"
         style.top = style.bottom = style.left = style.right = 0
